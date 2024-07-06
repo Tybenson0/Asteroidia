3cc41eae585b3f199a8dd4952807261dc9e958b3
tyler benson <tbens75@wgu.edu>
2024-07-05 20:25:15 -0700

    adjusted the front end for the predicitons

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -20,36 +20,16 @@ def predictions(request):
             absolute_magnitude = float(request.POST['absolute_magnitude'])
             estimated_diameter_min = float(request.POST['estimated_diameter_min'])
             estimated_diameter_max = float(request.POST['estimated_diameter_max'])
-            miss_distance_1 = float(request.POST['miss_distance_1'])
-            miss_distance_2 = float(request.POST['miss_distance_2'])
-            miss_distance_3 = float(request.POST['miss_distance_3'])
-            miss_distance_4 = float(request.POST['miss_distance_4'])
-            miss_distance_5 = float(request.POST['miss_distance_5'])
-            miss_distance_6 = float(request.POST['miss_distance_6'])
-            miss_distance_7 = float(request.POST['miss_distance_7'])
-            miss_distance_8 = float(request.POST['miss_distance_8'])
-            miss_distance_9 = float(request.POST['miss_distance_9'])
-            miss_distance_10 = float(request.POST['miss_distance_10'])
-
+            closest_approach_velocity = float(request.POST['closest_approach_velocity'])
             test_data_features = [
                 [
                     absolute_magnitude,
                     estimated_diameter_min,
                     estimated_diameter_max,
-                    miss_distance_1,
-                    miss_distance_2,
-                    miss_distance_3,
-                    miss_distance_4,
-                    miss_distance_5,
-                    miss_distance_6,
-                    miss_distance_7,
-                    miss_distance_8,
-                    miss_distance_9,
-                    miss_distance_10
+                    closest_approach_velocity
                 ]
             ]
 
-            # parsing the NEO data
             data = requests.get(asteroids_neows)  # request the NEO data
             data.raise_for_status()  # Raise an exception for HTTP errors
             near_earth_objects = data.json()  # Parse the JSON data
@@ -57,30 +37,32 @@ def predictions(request):
             targets = []
             for neo in near_earth_objects['near_earth_objects']:
                 features.append([
-                    neo['absolute_magnitude_h'],
-                    neo['estimated_diameter']['kilometers']['estimated_diameter_min'],
-                    neo['estimated_diameter']['kilometers']['estimated_diameter_max'],
-                    float(neo['close_approach_data'][1]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][2]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][3]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][4]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][5]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][6]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][7]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][8]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][9]['miss_distance']['kilometers']),
-                    float(neo['close_approach_data'][10]['miss_distance']['kilometers']),
-
-                    # Add more features as needed
+                    float(neo['absolute_magnitude_h']),
+                    float(neo['estimated_diameter']['kilometers']['estimated_diameter_min']),
+                    float(neo['estimated_diameter']['kilometers']['estimated_diameter_max']),
+                    float(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second'])
                 ])
-                targets.append(neo['is_potentially_hazardous_asteroid'])
 
-            features_train, features_test, target_train, target_test = train_test_split(features, targets,
-                                                                                        test_size=0.1, random_state=42)
+                # Define your own hazard metric based on asteroid size (estimated diameter)
+                # For example, let's classify asteroids with estimated diameter > 0.5 km as hazardous
+                if (float(neo['estimated_diameter']['kilometers']['estimated_diameter_min']) > 2
+                        and float(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second']) >= 10
+                            or neo['is_potentially_hazardous_asteroid'] == True or (float(neo['estimated_diameter']['kilometers']['estimated_diameter_min']) > 2
+                                and float(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second']) >= 20)):
+                    targets.append('Hazardous')
+                elif not neo['is_potentially_hazardous_asteroid']:
+                    targets.append('Non-Hazardous')
 
+            # Split data into training and testing sets
+            X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.4, random_state=42)
+
+            # Initialize linear regression model
+            model = LogisticRegression()
+            model.fit(X_train, y_train)
+
+            prediction = model.predict(test_data_features)
             # Return the prediction results to the template context
-            # return render(request, 'prediction.html', {'prediction': y_pred_list})
-            return JsonResponse({'predicted_results': features_train})
+            return render(request, 'prediction.html', {'prediction': prediction})
         except requests.exceptions.RequestException as e:
             return JsonResponse({'error': str(e)}, status=500)
     return render(request, 'prediction.html')
@@ -106,10 +88,13 @@ def model_testing(request):
             # Define your own hazard metric based on asteroid size (estimated diameter)
             # For example, let's classify asteroids with estimated diameter > 0.5 km as hazardous
             if (float(neo['estimated_diameter']['kilometers']['estimated_diameter_min']) > 2
-                    or float(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second']) >= 18
-                        or neo['is_potentially_hazardous_asteroid'] == True):
+                    and float(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second']) >= 18
+                    or neo['is_potentially_hazardous_asteroid'] == True or (
+                            float(neo['estimated_diameter']['kilometers']['estimated_diameter_min']) > 2
+                            and float(
+                        neo['close_approach_data'][0]['relative_velocity']['kilometers_per_second']) >= 18)):
                 targets.append('Hazardous')
-            elif  neo['is_potentially_hazardous_asteroid'] == False:
+            elif not neo['is_potentially_hazardous_asteroid']:
                 targets.append('Non-Hazardous')
 
         # Split data into training and testing sets
